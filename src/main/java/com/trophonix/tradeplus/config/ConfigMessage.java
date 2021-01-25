a52d0e96046546a480601e372da3a58d6c54bd34
zPirroZ3007 <giusepirro@hotmail.com>
2021-01-25 23:28:02 +0100

    Fix issue: https://forum.pirroproductions.com/index.php?threads/riapertura-richiesta-di-trade-con-passa.8425/

--- src/main/java/com/trophonix/tradeplus/config/ConfigMessage.java
+++ src/main/java/com/trophonix/tradeplus/config/ConfigMessage.java
@@ -10,64 +10,60 @@ import org.bukkit.entity.Player;
 
 public class ConfigMessage {
 
-  private String[] message;
-  private String onHover;
-  private String onClick;
+    private String[] message;
+    private String onHover;
+    private String onClick;
 
-  public ConfigMessage(ConfigurationSection yml, String key, String defaultText) {
-    String text = null;
-    if (yml.isString(key)) {
-      text = yml.getString(key);
-    } else if (yml.isConfigurationSection(key)) {
-      text = yml.getString(key + ".text");
-      onHover = yml.getString(key + ".hover");
-    }
-    if (text == null) text = defaultText;
-    if (text.contains("%NEWLINE%")) {
-      message = text.split("%NEWLINE%");
-      for (int i = 0; i < message.length; i++) {
-        message[i] = ChatColor.translateAlternateColorCodes('&', message[i]);
-      }
-    } else {
-      message = new String[] {ChatColor.translateAlternateColorCodes('&', text)};
-    }
-  }
-
-  public void send(CommandSender player, String... replacements) {
-    String hover = this.onHover;
-    if (hover != null)
-      for (int i = 0; i < replacements.length - 1; i += 2) {
-        hover = hover.replace(replacements[i], replacements[i + 1]);
-      }
-    for (String line : message) {
-      for (int i = 0; i < replacements.length - 1; i += 2) {
-        line = line.replace(replacements[i], replacements[i + 1]);
-      }
-
-      if (!player.hasPermission("tecnoroleplay.admin")) {
-        StringBuilder sb = new StringBuilder();
-
-        String[] spaces = line.split(" ");
-        for (String space : spaces) {
-          Player p = Bukkit.getPlayer(space);
-          if (p != null)
-            sb.append(TradeCommandHandler.hasPassaMontagna(p) ? "Anonimo" : space).append(" ");
-          else
-            sb.append(space).append(" ");
+    public ConfigMessage(ConfigurationSection yml, String key, String defaultText) {
+        String text = null;
+        if (yml.isString(key)) {
+            text = yml.getString(key);
+        } else if (yml.isConfigurationSection(key)) {
+            text = yml.getString(key + ".text");
+            onHover = yml.getString(key + ".hover");
         }
-        line = sb.toString();
-      }
-
-      if (onHover == null && onClick == null) {
-        player.sendMessage(line);
-      } else {
-        MsgUtils.send((Player) player, hover, onClick, line);
-      }
+        if (text == null) text = defaultText;
+        if (text.contains("%NEWLINE%")) {
+            message = text.split("%NEWLINE%");
+            for (int i = 0; i < message.length; i++) {
+                message[i] = ChatColor.translateAlternateColorCodes('&', message[i]);
+            }
+        } else {
+            message = new String[]{ChatColor.translateAlternateColorCodes('&', text)};
+        }
     }
+
+    public void send(CommandSender player, String... replacements) {
+        String hover = this.onHover;
+        if (hover != null) {
+          hover = getString(hover, replacements);
+        }
+      for (String line : message) {
+        line = getString(line, replacements);
+
+        if (onHover == null && onClick == null) {
+                player.sendMessage(line);
+            } else {
+                MsgUtils.send((Player) player, hover, onClick, line);
+            }
+        }
+    }
+
+  private String getString(String hover, String[] replacements) {
+    for (int i = 0; i < replacements.length - 1; i += 2) {
+        if (replacements[i].contains("%PLAYER")) {
+            Player p = Bukkit.getPlayer(replacements[i + 1]);
+            if (p != null)
+                hover = hover.replace(replacements[i], TradeCommandHandler.hasPassaMontagna(p) ? "Anonimo" : p.getName());
+            else
+                hover = hover.replace(replacements[i], replacements[i + 1]);
+        } else hover = hover.replace(replacements[i], replacements[i + 1]);
+    }
+    return hover;
   }
 
   public ConfigMessage setOnClick(String command) {
-    onClick = command;
-    return this;
-  }
+        onClick = command;
+        return this;
+    }
 }
